4031145ab6fc4bb3c54093243238a3259bf608db
trauts1234 <stu-junk@outlook.com>
2025-02-07 12:07:04 +0000

    added operator overloading for +, -, unary -, +=, -=

--- memory_size.rs
+++ memory_size.rs
@@ -1,6 +1,4 @@
-use std::ops::{Add, AddAssign};
-
-
+use std::ops::{Add, Sub, Neg, AddAssign, SubAssign};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct MemoryLayout {
@@ -49,36 +47,17 @@ impl MemoryLayout {
     }
 
     /**
+     * returns true if the size suggested by this MemorySize in bits is greater than 0
+     */
+    pub fn is_positive(&self) -> bool{
+        return self.size_bits > 0;
+    }
+
+    /**
      * Calculate the size suggested by this MemorySize in bits
      */
     pub fn size_bits(&self) -> isize{
         self.size_bits
-    }
-
-    pub fn increment_by(&mut self, rhs: &MemoryLayout){
-        self.size_bits += rhs.size_bits;
-    }
-
-
-}
-
-impl MemoryLayout {
-    /**
-     * Adds the memory size of the paramaters
-     */
-    pub fn add(lhs: &MemoryLayout, rhs: &MemoryLayout) -> MemoryLayout{
-        if lhs.size_bits.is_negative() || rhs.size_bits.is_negative(){
-            panic!("tried to add memory sizes, with one arg being a negative size")
-        }
-        MemoryLayout::from_bits(lhs.size_bits+rhs.size_bits)
-    }
-    /**
-     * Calculates a memory size with -original number of bits
-     * i.e 64 bit becomes -64 bit
-     * This is used for offsets where the offset may need to be negative
-     */
-    pub fn negate(lhs: &MemoryLayout) -> MemoryLayout{
-        MemoryLayout::from_bits(-lhs.size_bits)
     }
 
     pub fn safe_cast_to_unsigned(x: isize) -> usize {
@@ -94,5 +73,54 @@ impl MemoryLayout {
         } else {
             x as isize
         }
+    }
+
+}
+
+impl Add for MemoryLayout {
+    type Output = MemoryLayout;
+
+    fn add(self, rhs: MemoryLayout) -> MemoryLayout {
+        if self.size_bits.is_negative() || rhs.size_bits.is_negative(){
+            eprintln!("tried to add memory sizes, with one arg being a negative size")
+        }
+        MemoryLayout::from_bits(self.size_bits+rhs.size_bits)
+    }
+}
+
+impl AddAssign for MemoryLayout {
+    fn add_assign(&mut self, rhs: MemoryLayout) {
+        if rhs.size_bits.is_negative(){
+            eprintln!("adding negative size to memory size")
+        }
+        self.size_bits += rhs.size_bits;
+    }
+}
+
+impl Sub for MemoryLayout {
+    type Output = MemoryLayout;
+
+    fn sub(self, rhs: MemoryLayout) -> MemoryLayout {
+        if rhs.size_bits.is_negative(){
+            eprintln!("subtracting negative size from memory size")
+        }
+        MemoryLayout::from_bits(self.size_bits-rhs.size_bits)
+    }
+}
+
+impl SubAssign for MemoryLayout {
+    fn sub_assign(&mut self, rhs: MemoryLayout) {
+        if rhs.size_bits.is_negative(){
+            eprintln!("subtracting negative size from memory size")
+        }
+        self.size_bits -= rhs.size_bits;
+    }
+}
+
+impl Neg for MemoryLayout {
+    type Output = MemoryLayout;
+
+    fn neg(self) -> MemoryLayout {
+        MemoryLayout::from_bits(-self.size_bits)
     }
 }
