b00ec5a2777acd3f9d2e2b222783d54e4a4d0edd
trauts1234 <stu-junk@outlook.com>
2025-04-15 15:29:59 +0100

    made everything derived functions

--- memory_size.rs
+++ memory_size.rs
@@ -1,12 +1,17 @@
 mod tests;
 
-use std::{fmt::Display, iter::Sum, ops::{Add, AddAssign, Sub, SubAssign}};
+use std::fmt::Display;
 
+use derive_more::{Add, Sub, Sum, AddAssign, SubAssign};
 use humansize::{format_size, BaseUnit, FormatSizeOptions, Kilo};
 
 const BITS_IN_BYTE: u64 = 8;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+#[derive(
+    PartialEq, PartialOrd, Ord, Eq,
+    Clone, Copy, Hash, Debug,
+    Add, Sub, Sum, AddAssign, SubAssign
+)]
 pub struct MemorySize {
     size_bits: u64
 }
@@ -61,117 +66,6 @@ impl MemorySize {
         self.size_bits
     }
 
-}
-
-impl Add for MemorySize {
-    type Output = MemorySize;
-
-    /**
-     * adds the size in bytes under the hood
-     * so may panic!() on overflow only on debug builds
-     */
-    fn add(self, rhs: MemorySize) -> MemorySize {
-        
-        MemorySize::from_bits(
-            self.size_bits.checked_add(rhs.size_bits).expect("addition of MemorySize overflowed")
-        )
-    }
-}
-
-impl AddAssign for MemorySize {
-    /**
-     * finds the total size of self and rhs
-     * panics if:
-     * size_bits + rhs.size_bits > u64::MAX
-     */
-    fn add_assign(&mut self, rhs: MemorySize) {
-        self.size_bits = self.size_bits.checked_add(rhs.size_bits).expect("addition-assignment of MemorySize overflowed");
-    }
-}
-
-impl Sub for MemorySize {
-    type Output = MemorySize;
-
-    /**
-     * finds the differences in size represented by self and rhs
-     * panics if:
-     * size_bits - rhs.size_bits < 0
-     */
-    fn sub(self, rhs: MemorySize) -> MemorySize {
-        MemorySize::from_bits(
-            self.size_bits.checked_sub(rhs.size_bits).expect("subtraction of MemorySize underflowed")
-        )
-    }
-}
-
-impl SubAssign for MemorySize {
-    /**
-     * finds the differences in size represented by self and rhs
-     * panics if:
-     * size_bits - rhs.size_bits < 0
-     */
-    fn sub_assign(&mut self, rhs: MemorySize) {
-        self.size_bits = self.size_bits.checked_sub(rhs.size_bits).expect("subtraction-assignment of MemorySize underflowed");
-    }
-}
-impl Sum for MemorySize {
-    /**
-     * finds the total size represented by the MemorySizes in the iterator
-     * panics:
-     * if the sum overflows
-     */
-    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(MemorySize::new(), |acc, x| acc + x)
-    }
-}
-
-impl PartialOrd for MemorySize {
-    /**
-     * compares the size of each memory layout's size in bytes
-     */
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.size_bits.partial_cmp(&other.size_bits)
-    }
-}
-impl Ord for MemorySize {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.size_bits.cmp(&other.size_bits)
-    }
-    
-    /**
-     * returns the largest size represented by the two MemorySizes
-     */
-    fn max(self, other: Self) -> Self
-    where
-        Self: Sized,
-    {
-        std::cmp::max_by(self, other, Ord::cmp)
-    }
-    
-    /**
-     * returns the smallest size represented by the two MemorySizes
-     */
-    fn min(self, other: Self) -> Self
-    where
-        Self: Sized,
-    {
-        std::cmp::min_by(self, other, Ord::cmp)
-    }
-    
-    /**
-     * clamps the size in bytes to between min.size_bytes() and max.size_bytes()
-     * panics:
-     * if min > max
-     */
-    fn clamp(self, min: Self, max: Self) -> Self
-    where
-        Self: Sized,
-    {
-        assert!(min <= max);
-        MemorySize {
-            size_bits: self.size_bits.clamp(min.size_bits, max.size_bits)
-        }
-    }
 }
 
 impl Display for MemorySize {
